6c55b8d036080c01f96bb86be811e9f589fd7532
maxrangel <lik4ever@hotmail.com>
2022-04-06 21:45:38 -0500

    exercise 2, manipulate computer choice

--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 const btnStart = document.getElementById('btn-start');
 const btnStop = document.getElementById('btn-stop');
 const btnReset = document.getElementById('btn-reset');
+const btnRock = document.getElementById('btn-rock');
+const btnPaper = document.getElementById('btn-paper');
+const btnScissors = document.getElementById('btn-scissors');
+const playerChoice = document.getElementById('player-1-option');
+const computerChoice = document.getElementById('computer-choice');
+
 const playerCards = document.querySelectorAll('.player-card');
 const timer = document.getElementById('timer');
 
 // Variables
 const game = {
 	started: false,
+	playerChoice: 'paper',
+	remainingSeconds: 5,
 };
 
 // Default parameters
 const startTimer = (seconds = 5) => {
 	setTimeout(() => {
 		const remainingSeconds = seconds - 1;
+		game.remainingSeconds = remainingSeconds;
 
 		// Recursividad / Recursivity
 		if (remainingSeconds >= 0) {
@@ -22,6 +31,36 @@ const startTimer = (seconds = 5) => {
 		}
 	}, 1000);
 };
+
+const startRandomComputerChoice = () => {
+	setTimeout(() => {
+		// Generate random number between 1-3
+		// 1 - Rock
+		// 2 - Paper
+		// 3 - Scissors
+		// Change the computer choice based on the random number generated
+		// When timer reaches to 0, stop random choice
+	}, 1000);
+};
+
+const startGame = () => {
+	game.started = true;
+
+	btnStart.classList.add('disabled');
+	btnStop.classList.remove('disabled');
+	btnReset.classList.add('disabled');
+
+	// Start random computer choice
+	startRandomComputerChoice();
+};
+
+const removeSelectedOption = () => {
+	const currentSelected = document.querySelector('.selected');
+
+	if (currentSelected) currentSelected.classList.remove('selected');
+};
+
+// Events listeners
 
 // Callback/anonymous functions
 btnStart.addEventListener('click', () => {
@@ -33,12 +72,26 @@ btnStart.addEventListener('click', () => {
 		element.classList.remove('hidden');
 	});
 
-	game.started = true;
-	btnStart.classList.add('disabled');
-	btnStop.classList.remove('disabled');
-	btnReset.classList.add('disabled');
-
+	startGame();
 	startTimer();
+});
+
+btnRock.addEventListener('click', () => {
+	removeSelectedOption();
+	btnRock.classList.add('selected');
+	playerChoice.src = './assets/rock.png';
+});
+
+btnPaper.addEventListener('click', () => {
+	removeSelectedOption();
+	btnPaper.classList.add('selected');
+	playerChoice.src = './assets/paper.png';
+});
+
+btnScissors.addEventListener('click', () => {
+	removeSelectedOption();
+	btnScissors.classList.add('selected');
+	playerChoice.src = './assets/scissors.png';
 });
 
 // 1s = 1000 ms
